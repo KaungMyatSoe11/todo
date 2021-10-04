c10f9fa3d85c8dac42334ca59ba974427dfa18d4
Kaung Myat Soe <52688252+ZhanZhao30@users.noreply.github.com>
2021-10-04 19:59:29 +0630

    Update index.js

--- src/actions/todoactions/index.js
+++ src/actions/todoactions/index.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 export const addTask = (state) => {
   return (dispatch) => {
     console.log(state);
@@ -14,6 +16,16 @@ export const updateTask = (state) => {
     dispatch({
       type: "UPDATE_TASK",
       payload: state,
+    });
+  };
+};
+export const loadTask = () => {
+  return async (dispatch) => {
+    const res = await axios.get(".netlify/functions/gettodo");
+
+    dispatch({
+      type: "LOAD_TASK",
+      payload: res.data,
     });
   };
 };
