8d2352ae779db28648443917dacdd0a239ed41d8
Kaung Myat Soe <52688252+ZhanZhao30@users.noreply.github.com>
2021-10-04 19:59:37 +0630

    Update index.js

--- src/container/Home/index.js
+++ src/container/Home/index.js
@@ -1,11 +1,21 @@
 /* eslint-disable array-callback-return */
-import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { bindActionCreators } from "redux";
+import { todoactions } from "../../actions";
 import ToDoForm from "../../components/ToDoForm";
 import ToDoList from "../../components/ToDoList";
+import gettodos from "../../utils/api/gettodos";
 
 const Home = () => {
   const state = useSelector((state) => state);
   console.log(state.todo);
+  const dispatch = useDispatch();
+  const {  loadTask } = bindActionCreators(todoactions, dispatch);
+
+  useEffect(() => {
+    loadTask();
+  }, []);
 
   return (
     <div className="container">
@@ -13,7 +23,7 @@ const Home = () => {
       {state.todo.length > 0 ? (
         <>
           {state.todo.map((item) => (
-            <ToDoList todo={item} key={item.text} />
+            <ToDoList todo={item} key={item._id} />
           ))}
         </>
       ) : (
