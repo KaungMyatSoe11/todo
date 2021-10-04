c7107b1746fd8386b27a4766543066b6a576b1fc
Kaung Myat Soe <52688252+ZhanZhao30@users.noreply.github.com>
2021-10-04 19:59:43 +0630

    Update store.js

--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
 import { applyMiddleware, compose, createStore } from "redux";
 import { Reduxers } from "./Reducers";
 import thunk from "redux-thunk";
+import gettodos from "./utils/api/gettodos";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 export const store = createStore(
