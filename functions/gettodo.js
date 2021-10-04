b8856ac18f8dbc5fb06678775990236a7ae2a234
Kaung Myat Soe <52688252+ZhanZhao30@users.noreply.github.com>
2021-10-04 20:37:15 +0630

    Update gettodo.js

--- functions/gettodo.js
+++ functions/gettodo.js
@@ -1,15 +1,15 @@
-const connectToDataBase = require("./utils/connectToDataBase");
+// const connectToDataBase = require("./utils/connectToDatabase");
 
-module.exports.handler = async () => {
-  const db = await connectToDataBase();
-  const data = await db.collection("tasks").find({}).toArray();
-  console.log(data);
+// module.exports.handler = async () => {
+//   const db = await connectToDataBase();
+//   const data = await db.collection("tasks").find({}).toArray();
+//   console.log(data);
 
-  return {
-    statusCode: 200,
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  };
-};
+//   return {
+//     statusCode: 200,
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify(data),
+//   };
+// };
